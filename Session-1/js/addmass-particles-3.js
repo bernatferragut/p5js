96f3b2509246265ff6b1ad6022f56c83afdf32f1
Bernat Ferragut <elbernat@gmail.com>
2017-11-12 11:14:40 -0500

    gravity applied
    Force of gracity appleid to two diferent objects

--- Session-1/js/addmass-particles-3.js
+++ Session-1/js/addmass-particles-3.js
@@ -1,46 +1,63 @@
 // PS5
+let particle1;
+let particle2;
+
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
-    particle = new Particle(width/2, 0);
+    particle1 = new Particle(400, 100, 3);
+    particle2 = new Particle(1000, 100, 1);
 }
 
 function draw() {
-    // 1st external force affects the particle acc.
-    let gravity = createVector(0, 0.01); 
-    particle.applyForce(gravity);
-    // 2nd external force affects the particle acc.
-    if (mouseIsPressed) {
-        let wind = createVector(0.001,0);
-        particle.applyForce(wind);
+    background(100)
+    // External forces affects the particle acc.
+    let wind = createVector(0.01,0);
+
+    let gravity1 = createVector(0, 0.02 * particle1.mass);
+    let gravity2 = createVector(0, 0.02 * particle2.mass);
+    
+    particle1.applyForce(gravity1);
+    particle2.applyForce(gravity2);
+
+    if(mouseIsPressed) {
+        particle1.applyForce(wind);
+        particle2.applyForce(wind);
     }
-    // Physics: creation, updates, exceptions
-    particle.createParticle();
-    particle.updateParticle();
-    particle.edges();    
+
+    // Particle 1
+    particle1.createParticle();
+    particle1.updateParticle();
+    particle1.edges();    
+    
+    // Particle 2
+    particle2.createParticle();
+    particle2.updateParticle();
+    particle2.edges(); 
 }
 
 // Classes
 class Particle {
-    constructor(x,y){
+    constructor(x,y,m){
         this.pos = createVector(x, y);
         this.vel = createVector(0, 0);
         this.acc = createVector(0, 0);
+        this.mass = m;
     }
 
     createParticle() {
-        background(100);
         fill(0);
-        ellipse(this.pos.x, this.pos.y, 20, 20);
+        ellipse(this.pos.x, this.pos.y, this.mass * 10, this.mass * 10);
     }
 
     applyForce(force) {
-        this.acc.add(force);
+        let f = force.copy(); // se each particle can have it's own force.
+        f.div(this.mass);
+        this.acc.add(f);
     }
 
     updateParticle() {  
         this.vel.add(this.acc);
         this.pos.add(this.vel);
-        //this.acc.set(0,0); // start again to 0 in each frame;
     }
 
     edges() {
